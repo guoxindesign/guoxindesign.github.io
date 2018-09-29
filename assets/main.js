7a3e6878e180d9d1f1bc7bad8a00e7efeee8784c
Liu Lantao <liulantao@gmail.com>
2018-09-29 18:20:35 +0800

    chrome scroll fix
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,7 @@ layout: nil
 var target = document.getElementsByClassName("navbar")[0];
 
 document.onscroll = function(e){
-  var percent = document.body.scrollTop / 200;
+  var percent = Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 200;
 
   if (percent > 0.95) { percent = 0.95; }
   if (percent < 0) { percent = 0.0; }
