19617e0906d1e6f83f3e53c287da85789eec5d21
Liu Lantao <liulantao@gmail.com>
2018-09-29 16:06:13 +0800

    scroll
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
 ---
 layout: nil
 ---
+
+var target = document.getElementsByClassName("navbar")[0];
+
+document.onscroll = function(e){
+  var percent = document.body.scrollTop / 500;
+
+  if (percent > 0.95) { percent = 0.95; }
+  if (percent < 0) { percent = 0.0; }
+
+  target.style.backgroundColor = 'rgba(255, 255, 255, ' + percent + ')';
+};
